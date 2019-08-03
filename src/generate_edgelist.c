e338fdb826fd63b9a9ec7d254e4f7ec475ce8368
rwang067 <wangrui1994@126.com>
2019-08-03 17:02:51 +0800

    for

--- src/generate_edgelist.c
+++ src/generate_edgelist.c
@@ -150,7 +150,8 @@ int main(int argc, char** argv) {
 					//write files
 					FILE *fp;
 					fp=fopen(filename,"w");
-					for(int i = 0; i < edge_count; i++)
+					int i;
+					for(i = 0; i < edge_count; i++)
 						fprintf(fp,"%d %d\n",actual_buf[i].v0,actual_buf[i].v1 );
 					fclose(fp);
 
