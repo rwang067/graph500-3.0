89b37c0bca26ac9dfabe3f9cda41f641cb9d68d2
rwang067 <wangrui1994@126.com>
2019-08-03 16:26:43 +0800

    generate unsorted edgelist

--- generator/graph_generator.h
+++ generator/graph_generator.h
@@ -41,6 +41,7 @@ static inline int64_t get_v1_from_edge(const packed_edge* p) {
 }
 
 static inline void write_edge(packed_edge* p, int64_t v0, int64_t v1) {
+  printf("write_edge_48 :%ld %ld\n", v0, v1);
   p->v0_low = (uint32_t)v0;
   p->v1_low = (uint32_t)v1;
   p->high = (uint32_t)(((v0 >> 32) & 0xFFFF) | (((v1 >> 32) & 0xFFFF) << 16));
@@ -49,21 +50,22 @@ static inline void write_edge(packed_edge* p, int64_t v0, int64_t v1) {
 #else
 
 typedef struct packed_edge {
-  int64_t v0;
-  int64_t v1;
+  int32_t v0;
+  int32_t v1;
 } packed_edge;
 
-static inline int64_t get_v0_from_edge(const packed_edge* p) {
+static inline int32_t get_v0_from_edge(const packed_edge* p) {
   return p->v0;
 }
 
-static inline int64_t get_v1_from_edge(const packed_edge* p) {
+static inline int32_t get_v1_from_edge(const packed_edge* p) {
   return p->v1;
 }
 
 static inline void write_edge(packed_edge* p, int64_t v0, int64_t v1) {
-  p->v0 = v0;
-  p->v1 = v1;
+  printf("write_edge_32 :%ld %ld\n", v0, v1);
+  p->v0 = (uint32_t)v0;
+  p->v1 = (uint32_t)v1;
 }
 
 #endif
